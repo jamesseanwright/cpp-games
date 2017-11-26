0521c6e90bb34d060a94dfdc28d0fea674149b50
James Wright <james@jamesswright.co.uk>
2017-11-26 14:51:29 +0000

    WIP use of DecorativeSprite for clouds

--- timber/src/Timber.cpp
+++ timber/src/Timber.cpp
@@ -9,15 +9,17 @@ int main() {
 
     DecorativeSprite bee = DecorativeSprite::createBee(window.getSize());
 
+    DecorativeSprite clouds [3] = {
+        DecorativeSprite::createCloud(window.getSize()),
+        DecorativeSprite::createCloud(window.getSize()),
+        DecorativeSprite::createCloud(window.getSize())
+    };
+
     Sprite spriteBackground;
     Sprite spriteTree;
-    Sprite spriteCloud1;
-    Sprite spriteCloud2;
-    Sprite spriteCloud3;
 
     Texture textureBackground;
     Texture textureTree;
-    Texture textureCloud;
 
     textureBackground.loadFromFile("graphics/background.png");
     spriteBackground.setTexture(textureBackground);
@@ -27,19 +29,6 @@ int main() {
     spriteTree.setTexture(textureTree);
     spriteTree.setPosition(810, 0);
 
-    textureCloud.loadFromFile("graphics/cloud.png");
-    spriteCloud1.setTexture(textureCloud);
-    spriteCloud2.setTexture(textureCloud);
-    spriteCloud3.setTexture(textureCloud);
-
-    bool cloud1Active = false;
-    bool cloud2Active = false;
-    bool cloud3Active = false;
-
-    float cloud1Speed = 0.0f;
-    float cloud2Speed = 0.0f;
-    float cloud3Speed = 0.0f;
-
     Clock clock;
 
     while (window.isOpen()) {
@@ -48,34 +37,21 @@ int main() {
         }
 
         Time deltaTime = clock.restart();
+        float deltaSeconds = deltaTime.asSeconds();
 
-        bee.next(deltaTime.asSeconds());
+        bee.next(deltaSeconds);
 
-        if (!cloud1Active) {
-            srand((int)time(0) * 10);
-            cloud1Speed = (rand() % 200);
-
-            srand((int)time(0) * 10);
-            float y = (rand() % 150);
-            spriteCloud1.setPosition(-200, y);
-            cloud1Active = true;
-        } else {
-            spriteCloud1.setPosition(
-                spriteCloud1.getPosition().x + cloud1Speed * deltaTime.asSeconds(),
-                spriteCloud1.getPosition().y
-            );
-
-            if (spriteCloud1.getPosition().x > 1920) {
-                cloud1Active = false;
-            }
+        for (DecorativeSprite cloud : clouds) {
+            cloud.next(deltaSeconds);
         }
 
         window.clear();
-
         window.draw(spriteBackground);
-        window.draw(spriteCloud1);
-        window.draw(spriteCloud2);
-        window.draw(spriteCloud3);
+
+        for (DecorativeSprite cloud : clouds) {
+            window.draw(cloud.getSprite());
+        }
+
         window.draw(spriteTree);
         window.draw(bee.getSprite());
 
