894d9cc907f1b9187b2f838cb34e708ebe1f618c
James Wright <james@jamesswright.co.uk>
2017-12-02 17:40:20 +0000

    Further ECS definition

--- timber/src/components/Component.hpp
+++ timber/src/components/Component.hpp
@@ -1,13 +1,21 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+#include "../systems/System.hpp";
+
+using namespace sf;
 using namespace std;
 
 class Component {
     private:
+        Component(System* system, string name);
+        System* system;
         string name;
+        void next(Entity* entity, float deltaSeconds);
 
     public:
         string getName();
+        static Component create(System* system, string name);
 };
 
 #pragma once
--- timber/src/systems/System.hpp
+++ timber/src/systems/System.hpp
@@ -1,3 +1,7 @@
 #pragma once
 
+class System {
+
+};
+
 #pragma once
